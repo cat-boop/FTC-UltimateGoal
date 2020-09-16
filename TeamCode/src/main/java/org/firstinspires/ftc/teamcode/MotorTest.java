7b30b1810ba36e1d65db530511c5a531e854a5a1
roman_sushenko <sushenkoroman2003@gmail.com>
2020-09-16 20:00:49 +0700

    add Motor and Servo Test, update Hardware and TeleOperator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -6,35 +6,26 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="test", group="Linear Opmode")
+@TeleOp(name="MotorTest", group="Linear Opmode")
 
 public class MotorTest extends LinearOpMode {
 
 
     @Override
     public void runOpMode() {
-        DcMotor testmotor2 = null;
-        DcMotor lrsmotor = null;
-        testmotor2 = hardwareMap.get(DcMotor.class, "testmotor2");
-        lrsmotor = hardwareMap.get(DcMotor.class, "lrsmotor");
+        DcMotor test_motor = null;
+
+        test_motor = hardwareMap.get(DcMotor.class, "test_motor");
+
 
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            double left, right;
-
             double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
 
-
-            left   = Range.clip(drive + turn, -1.0, 1.0) ;
-            right  = Range.clip(drive - turn, -1.0, 1.0) ;
-
-            testmotor2.setPower(drive);
-            lrsmotor.setPower(drive);
+            test_motor.setPower(drive);
         }
-        }
-
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -48,7 +48,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@TeleOp(name = "Servo", group = "Concept")
+@TeleOp(name = "ServoTest", group = "Concept")
 //@Disabled
 public class ServoTest extends LinearOpMode {
 
