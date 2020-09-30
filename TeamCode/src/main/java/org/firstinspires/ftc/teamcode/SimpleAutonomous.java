c8e882337790f740b899de4b202cad3441c2dafb
roman_sushenko <sushenkoroman2003@gmail.com>
2020-09-30 18:44:11 +0700

    tested PID, find coefficients

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonomous.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class SimpleAutonomous extends LinearOpMode {
 
     Hardware hardware = new Hardware();
+    Gyroscope gyroscope = new Gyroscope();
     ElapsedTime timer = new ElapsedTime();
 
     double time_start;
@@ -15,24 +16,45 @@ public class SimpleAutonomous extends LinearOpMode {
     @Override
     public void runOpMode() {
         hardware.init(hardwareMap);
+        gyroscope.init(hardwareMap);
 
         waitForStart();
         timer.reset();
+
 
         for (int i = 0; i < 4 && !isStopRequested(); i++) {
             //движение прямо
             driveForTime(4, 0.5, 0, 0);
 
             //поворот на 90 градусов
-            driveForTime(2, 0, 0.5, 0);
+            turnForTime(90 * (i + 1));
         }
+
+
         hardware.setPower(0, 0, 0);
     }
 
+
     public void driveForTime(double time, double move, double turn, double sideways) {
         time_start = timer.seconds();
-        while (timer.seconds() - time_start < time && !isStopRequested()) {
+        while (timer.seconds() - time_start < time && opModeIsActive()) {
             hardware.setPower(move, turn, sideways);
+        }
+    }
+
+    public void turnForTime(double target_angle) {
+        double current_angle = gyroscope.getAngle();
+
+        while (  Math.abs(target_angle - current_angle) < 2 &&  opModeIsActive()) {
+
+            double power = gyroscope.turnTo(current_angle, target_angle);
+            current_angle = gyroscope.getAngle();
+            hardware.setPower(0, power, 0);
+
+            telemetry.addData("Current angle", current_angle);
+            telemetry.addData("Error", gyroscope.format(target_angle, current_angle));
+            telemetry.addData("Power", power);
+            telemetry.update();
         }
     }
 }
