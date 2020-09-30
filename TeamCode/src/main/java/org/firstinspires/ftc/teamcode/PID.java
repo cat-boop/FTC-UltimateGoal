f4a30e5315af3455dfd297bd92d67dbf31e1f57a
roman_sushenko <sushenkoroman2003@gmail.com>
2020-09-30 18:40:08 +0700

    create simple PID regulator

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.Range;
+
+public class PID {
+    double kP, kI, kD;
+
+    double error = 0, past_error = Double.NaN;
+
+    public PID(double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+    }
+
+    public double apply(double error) {
+        // past_error = error;
+        return clipValue(iRegulator(error));
+    }
+
+    private double iRegulator(double error) { return error * kP; }
+
+    private double clipValue(double value) {
+        double min_negative_value = -1, max_negative_value = -0.1;
+        double max_positive_value = 1, min_positive_value = 0.1;
+        return (value > 0) ? Range.clip(value, min_positive_value, max_positive_value) : Range.clip(value, min_negative_value, max_negative_value);
+    }
+}
