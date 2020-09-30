dfc74c9a6440712b1268051b1a1363cc367677b9
roman_sushenko <sushenkoroman2003@gmail.com>
2020-09-30 18:47:05 +0700

    add Sample Vision programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/EasyOpenCVExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/EasyOpenCVExample.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;/*
+package org.firstinspires.ftc.teamcode.vision;/*
  * Copyright (c) 2020 OpenFTC Team
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
