28541cbb18cd05cad80faab6414db03a5df058d7
roman_sushenko <sushenkoroman2003@gmail.com>
2020-10-07 22:52:28 +0700

    autonomous for square movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAutonomous.java
@@ -24,10 +24,12 @@ public class SimpleAutonomous extends LinearOpMode {
 
         for (int i = 0; i < 4 && !isStopRequested(); i++) {
             //движение прямо
-            driveForTime(4, 0.5, 0, 0);
+            driveForTime(2, 0.5, 0, 0);
 
             //поворот на 90 градусов
             turnForTime(90 * (i + 1));
+
+            sleep(200);
         }
 
 
@@ -39,22 +41,32 @@ public class SimpleAutonomous extends LinearOpMode {
         time_start = timer.seconds();
         while (timer.seconds() - time_start < time && opModeIsActive()) {
             hardware.setPower(move, turn, sideways);
+            telemetry.addData("Current time", timer.seconds());
+            telemetry.update();
         }
+        hardware.setPower(0, 0, 0);
     }
 
     public void turnForTime(double target_angle) {
         double current_angle = gyroscope.getAngle();
+        ElapsedTime time_at_target = new ElapsedTime();
 
-        while (  Math.abs(target_angle - current_angle) < 2 &&  opModeIsActive()) {
+        while ( (Math.abs(gyroscope.format(target_angle, current_angle)) > 2 || time_at_target.seconds() < 1) && opModeIsActive()) {
+
+            if (Math.abs(gyroscope.format(target_angle, current_angle)) > 4) {
+                time_at_target.reset();
+            }
 
             double power = gyroscope.turnTo(current_angle, target_angle);
             current_angle = gyroscope.getAngle();
             hardware.setPower(0, power, 0);
 
             telemetry.addData("Current angle", current_angle);
+            telemetry.addData("Time at target", time_at_target.seconds());
             telemetry.addData("Error", gyroscope.format(target_angle, current_angle));
             telemetry.addData("Power", power);
             telemetry.update();
         }
+        hardware.setPower(0, 0, 0);
     }
 }
