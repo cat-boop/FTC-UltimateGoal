d5651c44f1ac5afb5a9c5921fb461bd5e4444638
roman_sushenko <sushenkoroman2003@gmail.com>
2020-10-19 20:56:13 +0700

    test new magnical encoder (fail)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AS5600.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
+@TeleOp
+public class AS5600 extends LinearOpMode {
+
+    AnalogInput magneticEncoder;
+
+    double positionMagneticEncoder;
+
+    @Override
+    public void runOpMode() {
+
+        magneticEncoder = hardwareMap.get(AnalogInput.class, "magneticEncoder");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            positionMagneticEncoder = magneticEncoder.getVoltage();
+
+            telemetry.addData("Many shit:", positionMagneticEncoder);
+            telemetry.update();
+        }
+    }
+}
