9142163b341483fc82c9df0ed4e12fb99051647a
roman_sushenko <sushenkoroman2003@gmail.com>
2020-10-19 20:44:50 +0700

    test built-in encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -6,26 +6,45 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="MotorTest", group="Linear Opmode")
-
+@TeleOp(name = "MotorTest")
 public class MotorTest extends LinearOpMode {
 
+    DcMotor test_motor = null;
+
+    static final double     COUNTS_PER_MOTOR        = 288;
+    static final double     COUNTS_PER_ROTATION    =   4;
 
     @Override
     public void runOpMode() {
-        DcMotor test_motor = null;
-
         test_motor = hardwareMap.get(DcMotor.class, "test_motor");
+
+        test_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        test_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+        encoderDrive(0.1, 1);
+    }
 
-            double drive = -gamepad1.left_stick_y;
+    public void encoderDrive(double speed, double number_of_rotation) {
+        int newTarget;
+        if (opModeIsActive()) {
+            newTarget = test_motor.getCurrentPosition() + (int)(number_of_rotation * COUNTS_PER_MOTOR);
+            test_motor.setTargetPosition(newTarget);
 
-            test_motor.setPower(drive);
+            test_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            test_motor.setPower(Math.abs(speed));
+
+            while (opModeIsActive() && test_motor.isBusy()) {
+                telemetry.addData("Current position", test_motor.getCurrentPosition());
+                telemetry.update();
+            }
+
+            test_motor.setPower(0);
+
+            test_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
 }
