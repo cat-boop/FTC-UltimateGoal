e75720512d82c19d5314cc561bac7be3aa95c5c0
roman_sushenko <sushenkoroman2003@gmail.com>
2020-12-29 22:37:26 +0700

    commit to test notification

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+//test for notification
 public class Hardware {
 
     public final double MINOR_MIN = 0, MINOR_MAX = 0.2;
