fee62d43ae30a958a476860f94ccd2043c94838b
Zakhar <gapezar>
2021-02-05 17:26:19 +0700

    wobble regulator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeployWobble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeployWobble.java
@@ -7,28 +7,36 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
 public class DeployWobble extends LinearOpMode {
-    public void runOpMode(){
+    public void runOpMode() {
 
         DcMotor wobble = null;
         wobble = hardwareMap.get(DcMotor.class, "wobble");
 
-        wobble.setMode (DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        wobble.setTargetPosition(-228);
+        double EP = 0;
+        double K = 0;
+
+        wobble.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wobble.setTargetPosition(-80);
         wobble.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        wobble.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //wobble.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
 
-        while(opModeIsActive()) {
+        while (opModeIsActive()) {
             //tick -248
 
-            wobble.setPower(0.5);
+            if (gamepad1.right_bumper) K += 0.1;
+            else if (gamepad1.left_bumper) K -= 0.1;
 
+            EP = wobble.getCurrentPosition() - 58;
+
+            wobble.setPower(EP * K);
+
+            telemetry.addData("K:", K);
             telemetry.addData("tick", wobble.getCurrentPosition());
             telemetry.update();
-            }
-
         }
     }
+}
 
