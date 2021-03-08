db8fdc47c7263d5945656fcfad97772da8f6f56c
Роман Сущенко <sushenkoroman2003@gmail.com>
2021-03-08 14:02:30 +0700

    try tuning the pidf for ourselves

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDFValues.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class PIDFValues {
+    public static double kP = 0, kI = 17, kD = 0, kF = 0;
+}
